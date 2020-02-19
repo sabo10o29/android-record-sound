952b3e1105f270dfb2302a861d3c7b831f072311
sabo10o29 <alex29sabo10@gmail.com>
2020-02-19 22:39:20 +0900

    First commit

---
+++ app/src/main/java/com/sabo10/soundrecorder/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.sabo10.soundrecorder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var startbtn : Button
+    private lateinit var stopbtn : Button
+    private lateinit var playbtn : Button
+    private lateinit var stopplay : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+
+    }
+}
